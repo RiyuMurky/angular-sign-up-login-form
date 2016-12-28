b8d4248a4649bd03ca514b2eaab4cab9ef86c66f
Vladimir Chepkasov <v.chepkasov@mfisoft.ru>
2016-12-28 16:01:54 +0300

    new gh-pages version

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{330:function(n,o){},599:function(n,o,c){n.exports=c(330)}},[599]);
+webpackJsonp([1,2],{335:function(n,o){},617:function(n,o,c){n.exports=c(335)}},[617]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
